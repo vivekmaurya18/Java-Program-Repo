52d654d45fd9b33e212cb7d212358a11ed134e43
vivekmaurya <maurya9695v@gmail.com>
2026-04-30 23:34:04 +0530

    Add files via upload

---
+++ MultiDimentionalArray.java
@@ -0,0 +1,10 @@
+
+class MultiDimentionalArray {
+    public static void main(String[] args){
+
+        int[][] arr = { { 1, 2 }, 
+                       	{ 3, 4 } };
+      
+		System.out.println("a[1][1] : " + arr[1][1]);
+    }
+}

fb31a627595ade4ee850ed59431aa668fcfc0b27
vivekmaurya <maurya9695v@gmail.com>
2026-05-05 11:19:45 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,8 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr={54,32,3,2,1};
+        for(int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
